f04347cabd8b7094897057d5aa6ed0352b268bd8
hyperdbio <rladbstn9408@naver.com>
2023-11-23 10:10:40 +0900

    변수 교환

---
+++ variable2.py
@@ -0,0 +1,13 @@
+num1, num2 = 3, 4
+print("기존 변수값=",num1, num2)
+num1, num2 = num2, num1
+print("교환에 의한 변수값=",num1, num2)
+
+num1, num2 = 3, 4
+print("기존 변수값=",num1, num2)
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print("교환에 의한 변수값=",num1, num2)
